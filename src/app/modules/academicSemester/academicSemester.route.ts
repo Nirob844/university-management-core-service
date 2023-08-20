e9cc7d620d312d92a3be114708c3c64170fccfdc
Nirob Hasan <nirob427417@gmail.com>
2023-08-20 20:17:20 +0600

    academic semester

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validition';
+
+const router = express.Router();
+
+router.get('/:id', AcademicSemesterController.getDataById);
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  //auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  //auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
+);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
+router.get('/', AcademicSemesterController.getAllFromDB);
+
+export const AcademicSemesterRoute = router;
